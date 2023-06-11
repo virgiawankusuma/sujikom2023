83cd95c6f6506d8bf9f582b5ed950d931044b3dd
virgiawankusuma <virgiawan150@gmail.com>
2023-06-11 13:17:43 +0700

    ✨ feat: add login features

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -16,6 +16,10 @@ class Auth extends BaseController
 
     public function index()
     {
+        if(session()->get('email')) {
+            return redirect()->to('/admin');
+        } 
+        
         $data = [
             'title'=> 'Login | Kegiatan',
         ];
@@ -24,35 +28,47 @@ class Auth extends BaseController
     }
 
     public function login() {
+        
         $email = $this->request->getVar('email');
         $password = $this->request->getVar('password');
-
-        // $user = $this->db->get_where('user', ['email' => $email])->row();
-        $user = $this->userModel->getUser($email)->find();
-
-        dd($user);
-
-        if (password_verify($password, $user->password)) {
-            $sess = [
-                'email' => $user->email,
-                'role_id' => $user->role_id
-            ];
-
-            session()->set_userdata($sess);
-            // $this->session->set_userdata($sess);
-            return redirect()->back()->withInput();
+        
+        $user = $this->userModel->getUser($email)->first();
+        
+        // dd($user);
+        if($user) {
+            if (password_verify($password, $user['password'])) {
+                $session_login = [
+                    'email' => $user['email'],
+                ];
+                session()->set($session_login);
+    
+                session()->setFlashdata('message', 'Welcome back, ' . $user['nama']);
+    
+                return redirect()->to('/admin');
+            } else {
+                session()->setFlashdata('errorMessage', 'Password incorrect');
+                return redirect()->to('/login');
+            }
+        } else {
+            session()->setFlashdata('errorMessage', 'Email not registered');
+            return redirect()->to('/login');
         }
 
-        dd($email, $password);
     }
 
     public function register()
     {
         $data = [
-            'title'=> 'Register | Kegiatan',
+            'title' => 'Register | Kegiatan',
         ];
+
+        
 
         return view('auth/register', $data);
     }
     
+    public function logout() {
+        session()->remove('email');
+        return redirect()->to('/login');
+    }
 }
